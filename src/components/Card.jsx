f7ee7671626db2a91142412f1f865dbcc6fef6b3
Viraj Bhagat <virajbhagat03@gmail.com>
2025-06-25 20:42:40 +0530

    format document

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,45 +1,42 @@
-import React from 'react';
-import image1 from '../assets/img/image1.avif';
+import React from "react";
+import image1 from "../assets/img/image1.avif";
 import { LuLeafyGreen } from "react-icons/lu";
 import { GiChickenOven } from "react-icons/gi";
-import { useDispatch } from 'react-redux';
-import { AddItem } from '../redux/cartSlice';
-import { toast } from 'react-toastify';
+import { useDispatch } from "react-redux";
+import { AddItem } from "../redux/cartSlice";
+import { toast } from "react-toastify";
 
-function Card({name, image, id, price, type}) {
-
- let dispatch = useDispatch();
+function Card({ name, image, id, price, type }) {
+  let dispatch = useDispatch();
 
   return (
-    <div className='w-[300px] h-[350px] bg-white p-4 rounded-lg flex flex-col hover:border-2 border-green-300'>
-        <div className='w-full h-[60%] overflow-hidden rounded-lg shadow-lg'>
-            <img src={image} alt='' className='object-cover aspect-3/2 h-full' />
+    <div className="w-[300px] h-[350px] bg-white p-4 rounded-lg flex flex-col hover:border-2 border-green-300">
+      <div className="w-full h-[60%] overflow-hidden rounded-lg shadow-lg">
+        <img src={image} alt="" className="object-cover aspect-3/2 h-full" />
+      </div>
+      <div className="h-[40%] flex flex-col justify-end">
+        <div className="text-2xl font-semibold">{name}</div>
+        <div className="w-full flex justify-between items-center">
+          <div className="text-lg font-bold text-green-500">Rs {price} /-</div>
+          <div className="flex justify-center items-center gap-2 text-lg font-semibold text-green-500">
+            {type === "veg" ? <LuLeafyGreen /> : <GiChickenOven />}
+            <span>{type === "veg" ? "Veg" : "Non Veg"}</span>
+          </div>
         </div>
-        <div className='h-[40%] flex flex-col justify-end'>
-            <div className='text-2xl font-semibold'>
-                {name}
-            </div>
-            <div className='w-full flex justify-between items-center'>
-                <div className='text-lg font-bold text-green-500'>
-                    Rs {price} /-
-                </div>
-                <div className='flex justify-center items-center gap-2 text-lg font-semibold text-green-500'>
-                    {
-                        type === 'veg' ? <LuLeafyGreen /> : <GiChickenOven />
-                    }
-                    <span>
-                        {
-                            type === 'veg' ? 'Veg' : 'Non Veg'
-                        }
-                    </span>
-                </div>
-            </div>
-            <button className='w-full p-3 bg-green-400 text-white hover:bg-green-300 transition rounded-lg cursor-pointer font-bold' onClick={() => {dispatch(AddItem({id: id, name: name, price: price, img: image, qty: 1})); toast.success("Item added in cart.")}}>
-                Add to dish
-            </button>
-        </div>
+        <button
+          className="w-full p-3 bg-green-400 text-white hover:bg-green-300 transition rounded-lg cursor-pointer font-bold"
+          onClick={() => {
+            dispatch(
+              AddItem({ id: id, name: name, price: price, img: image, qty: 1 })
+            );
+            toast.success("Item added in cart.");
+          }}
+        >
+          Add to dish
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Card
+export default Card;
